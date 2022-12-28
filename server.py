ecf47b774d7f63286e6b0e86f84627c1aeb6cd86
ivan <iv.medvedev179@gmail.com>
2022-12-28 18:28:46 +0300

    initial version

---
+++ server.py
@@ -0,0 +1,69 @@
+import socket
+import struct
+import select
+from socketserver import ThreadingMixIn, TCPServer, StreamRequestHandler
+
+import logging
+
+HOST = "localhost"
+PORT = 1080
+
+class SocksProxy(StreamRequestHandler):
+    def get_user_id(self) -> str:
+        resulted_id = []
+        c = self.connection.recv(1)
+        while ord(c):
+            resulted_id.append(c.decode("utf-8"))
+
+        return ''.join(resulted_id)
+
+    @staticmethod
+    def begin_exchange(client, remote):
+        r, w, e = select.select([client, remote], [], [])
+        while True:
+            if client in r:
+                data = client.recv(4096)
+                if remote.send(data) <= 0:
+                    break
+
+            if remote in r:
+                data = remote.recv(4096)
+                if client.send(data) <= 0:
+                    break
+
+    def handle(self) -> None:
+        logging.info('Accepting connection from %s:%s' % self.client_address)
+
+        try:
+            # initiate connection
+            version = struct.unpack("!B", self.connection.recv(1))[0]
+            cmd = struct.unpack("!B", self.connection.recv(1))[0]
+            remote_port = struct.unpack('!H', self.connection.recv(2))[0]
+            remote_address = socket.inet_ntoa(self.connection.recv(4))
+            user_id = self.get_user_id()
+
+            # send acknowledge
+            self.connection.sendall(b'\x00')
+            self.connection.sendall(b'\x5a')
+            for i in range(6):
+                self.connection.sendall(b'\x00')
+
+            if cmd == 1:
+                remote = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                remote.connect((remote_address, remote_port))
+                bind_address = remote.getsockname()
+                logging.info('Connected to %s %s' % (remote_address, remote_port))
+                self.begin_exchange()
+
+            self.server.close_request(self.request)
+        except Exception as err:
+            logging.error(err)
+
+
+class AsyncTCPServer(ThreadingMixIn, TCPServer):
+    pass
+
+
+if __name__ == '__main__':
+    with AsyncTCPServer((HOST, PORT), SocksProxy) as server:
+        server.serve_forever()
